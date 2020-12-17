d9d4efdeef4b1fd0546ef2e6df1cc0046ce5ab19
Deasy Indrawati <indrawatideasy@gmail.com>
2020-12-17 23:12:44 +0700

    Add files via upload

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -15,6 +15,7 @@ from model import LSTMClassifier
 
 from utils import review_to_words, convert_and_pad
 
+
 def model_fn(model_dir):
     """Load the PyTorch model from the `model_dir` directory."""
     print("Loading model.")
@@ -46,6 +47,7 @@ def model_fn(model_dir):
     print("Done loading model.")
     return model
 
+
 def input_fn(serialized_input_data, content_type):
     print('Deserializing the input data.')
     if content_type == 'text/plain':
@@ -53,31 +55,34 @@ def input_fn(serialized_input_data, content_type):
         return data
     raise Exception('Requested unsupported ContentType in content_type: ' + content_type)
 
+
 def output_fn(prediction_output, accept):
     print('Serializing the generated output.')
     return str(prediction_output)
+
 
 def predict_fn(input_data, model):
     print('Inferring sentiment of input data.')
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
+
     if model.word_dict is None:
         raise Exception('Model has not been loaded properly, no word_dict.')
-    
+
     # TODO: Process input_data so that it is ready to be sent to our model.
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
+    words_ = review_to_words(input_data)
 
-    data_X = None
-    data_len = None
+    data_X, data_len = convert_and_pad(model.word_dict, words_)
+    # data_len = None
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
     data_pack = np.hstack((data_len, data_X))
     data_pack = data_pack.reshape(1, -1)
-    
+
     data = torch.from_numpy(data_pack)
     data = data.to(device)
 
@@ -86,7 +91,7 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
-    result = None
+    with torch.no_grad():
+        result = np.round(model.forward(data).numpy())
 
     return result
